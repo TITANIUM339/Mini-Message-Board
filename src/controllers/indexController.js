6b11a7ab7aeb53a4d09c35e93a9dedbfe4beab05
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-14 12:03:57 +0300

    Add message detail route

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,10 +1,21 @@
 import { getMessages } from "../models/messages.js";
 
 export default {
-    get(req, res) {
+    getIndex(req, res) {
         res.render("pages/index", {
             title: "Mini Message Board",
             messages: getMessages()
         });
     },
+    getMessage(req, res, next) {
+        const index = Number(req.params.index);
+        const messages = getMessages();
+
+        if (isNaN(index) || index < 0 || index >= messages.length) {
+            next();
+            return;
+        }
+
+        res.render("pages/message", { message: messages[index] });
+    }
 };
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -3,6 +3,7 @@ import controller from "../controllers/indexController.js";
 
 const router = Router();
 
-router.get("/", controller.get);
+router.get("/", controller.getIndex);
+router.get("/:index", controller.getMessage);
 
 export default router;
