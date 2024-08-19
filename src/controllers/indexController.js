d83d193a63d013d3d97b7c16694c1fc8a59c23d2
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-19 12:18:50 +0300

    Retrieve data from databse and handle errors

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,21 +1,59 @@
-import { getMessages } from "../models/messages.js";
+import { getMessage, getMessages } from "../models/queries.js";
+import { format } from "date-fns";
+import CustomError from "../helpers/CustomError.js";
 
 export default {
-    getIndex(req, res) {
-        res.render("pages/index", {
-            title: "Mini Message Board",
-            messages: getMessages(),
-        });
-    },
-    getMessage(req, res, next) {
-        const index = Number(req.params.index);
-        const messages = getMessages();
+    async getIndex(req, res, next) {
+        try {
+            const messages = await getMessages();
 
-        if (isNaN(index) || index < 0 || index >= messages.length) {
+            res.render("pages/index", {
+                title: "Mini Message Board",
+                messages,
+            });   
+        } catch (error) {
+            console.error(error);
+            
+            next(
+                new CustomError(
+                    "Internal Server Error",
+                    "Something unexpected has occurred. Try reloading the page.",
+                    500,
+                ),
+            );
+        }
+    },
+    async getMessage(req, res, next) {
+        const id = Number(req.params.id);
+
+        if (!isFinite(id) || id > (2 ** 32 - 1) || id < 1) {
             next("router");
             return;
         }
 
-        res.render("pages/message", { message: messages[index] });
+        try {
+            const message = await getMessage(id);
+
+            if (!message) {
+                next("router");
+                return;
+            }
+
+            message.date = format(message.date, "yyy-MM-dd HH:mm 'UTC'");
+
+            res.render("pages/message", {
+                message,
+            });
+        } catch (error) {
+            console.error(error);
+            
+            next(
+                new CustomError(
+                    "Internal Server Error",
+                    "Something unexpected has occurred. Try reloading the page.",
+                    500,
+                ),
+            );
+        }
     },
 };
