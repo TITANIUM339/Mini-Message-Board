51920b1ff3d454c3f51a93344abcd211ddb9319f
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-08-19 12:16:23 +0300

    Change route name

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,6 +4,6 @@ import controller from "../controllers/indexController.js";
 const router = Router();
 
 router.get("/", controller.getIndex);
-router.get("/:index", controller.getMessage);
+router.get("/:id", controller.getMessage);
 
 export default router;
